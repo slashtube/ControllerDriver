fa213b564336fe9c180fc22abd9c41696d04dc93
slashtube <tommy.ferpet6@gmail.com>
2024-08-23 19:08:40 +0200

    Modified rebind.sh

--- rebind.sh
+++ rebind.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-DEVICE="3-4.3:1.0"
+DEVICE="x-x.x:x.x"
 sudo bash -c "echo $DEVICE > /sys/bus/usb/drivers/usbhid/unbind"
 sudo bash -c "echo $DEVICE > /sys/bus/usb/drivers/GameCubeDriver/bind"
